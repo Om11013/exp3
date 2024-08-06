0dc4cdbc9008f0a5794cd739add9e9f112a9f739
Om <poonjaniom11@gmail.com>
2024-08-07 00:36:43 +0530

    Modified feature/hero-section branch

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Navbar'
+import Footer from './Footer'
+import HeroSection from './HeroSection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <h1>Hi There, Welcome to Om's Page</h1>
       <h2>Current Branch</h2>
+      <HeroSection/>
+      <Footer/>
     </>
   )
 }
---
+++ src/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+      <div>
+          You are in the Hero Section of Om's Website
+    </div>
+  )
+}
+
+export default HeroSection
